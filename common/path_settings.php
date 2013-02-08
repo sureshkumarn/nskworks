cabf983e3b12e3a7c448100af27eb661d4d5f934
unknown <snata2@BLRSNATA233528.sapient.com>
2013-02-08 12:27:08 +0530

    Initial Path Settings
    Configured paths for the work

---
+++ common/path_settings.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @Description : Common File for all the apps which stores the sitewide settings and paths
+ */
+$base_path = $_SERVER['DOCUMENT_ROOT'];
+$server_path =  $_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'];
+$common_path = $base_path.'/common';
+$css_path = $server_path.'/css';
+$js_path = $server_path.'/js';
+
+
+?>
